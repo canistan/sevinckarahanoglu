5a026a59f6d476e2afe60bc8d257e45ead220934
Şemsi Can Albayrak <semsicanalbayrak@gmail.com>
2026-07-13 16:12:54 +0300

    feat: add Google Tag Manager integration to the frontend layout

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -86,6 +86,19 @@ export default function RootLayout({
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaJSON) }}
         />
+        
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KRFSZWVS');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
+
         <Script src="https://www.googletagmanager.com/gtag/js?id=G-EJHPSB457S" strategy="afterInteractive" />
         <Script id="google-analytics" strategy="afterInteractive">
           {`
@@ -103,6 +116,17 @@ export default function RootLayout({
         </Script>
       </head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KRFSZWVS"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <Header />
         <main>{children}</main>
         <Footer />
